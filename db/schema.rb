4d5fb14e0f569bcb06cf00b2cd1833dc397d1988
kenken-9 <1508.parfum@gmail.com>
2021-04-26 18:48:22 +0900

    ✨Itemモデルの作成

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_25_062911) do
+ActiveRecord::Schema.define(version: 2021_04_26_091811) do
+
+  create_table "items", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "taste", null: false
+    t.integer "price", null: false
+    t.integer "protein", null: false
+    t.integer "lipid", null: false
+    t.integer "amount", null: false
+    t.text "impression", null: false
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_items_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nickname", null: false
@@ -26,4 +40,5 @@ ActiveRecord::Schema.define(version: 2021_04_25_062911) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "items", "users"
 end
