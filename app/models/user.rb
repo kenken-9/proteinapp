ecdfb3643d26bba3343f3823fbec62a068179c04
kenken-9 <1508.parfum@gmail.com>
2021-04-26 14:46:54 +0900

    🥅 エラー文の日本語を追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,10 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   with_options presence: true do
-    validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"を6文字以上の半角英数字で入力してください" }
     validates :nickname
     validates :profile
   end
+
+  validates :password, format: {with:/\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i, message:"を6文字以上の半角英数字で入力してください" }
 
 end
