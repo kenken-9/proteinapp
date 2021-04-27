c4e543d0b8a9a694e5357ea0cbfb10e115c5aac6
kenken-9 <1508.parfum@gmail.com>
2021-04-27 18:09:05 +0900

    🥅 １以上で入力指定のバリエーション

--- app/models/item.rb
+++ app/models/item.rb
@@ -7,9 +7,11 @@ class Item < ApplicationRecord
     validates :name
     validates :taste
     with_options format: {with: /\A[0-9]+\z/ , message: "を半角数字で入力してください"} do
-      validates :price
-      validates :amount
-      validates :protein
+      with_options inclusion: { in: 1..9999999, message: "を1以上で入力してください" } do
+        validates :price
+        validates :amount
+        validates :protein
+      end
       validates :lipid
     end
     validates :impression
