2da56d16516c366ee70efc24e2120698d1f025bf
kenken-9 <1508.parfum@gmail.com>
2021-04-30 16:55:38 +0900

    🎨 Bookmarkコントローラ作成

---
+++ app/controllers/bookmarks_controller.rb
@@ -0,0 +1,8 @@
+class BookmarksController < ApplicationController
+
+    
+
+   
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users
+
   root "items#index"
-  resources :items do
+
+  resources :items, shallow: true do
     resources :comments, only: [:create]
   end
 end
