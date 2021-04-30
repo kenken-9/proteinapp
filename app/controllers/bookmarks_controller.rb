279351617d8515cea3900aacc83e65dd6e0a7646
kenken-9 <1508.parfum@gmail.com>
2021-04-30 17:14:35 +0900

    🎨Bockmarksコントローラーの編集

--- app/controllers/bookmarks_controller.rb
+++ app/controllers/bookmarks_controller.rb
@@ -1,8 +1,22 @@
 class BookmarksController < ApplicationController
 
-    
+    def create
+        @item = Item.find(params[item_id])
+        @user = @item.user
+        current_user.bookmark(@item)
+        respond_to do |format|
+          format.html { redirect_to request.referrer || root_url }
+          format.js
+        end
+    end
 
-   
-
+    def destroy
+        @item = Item.find(params[:item_id])
+        current_user.bookmark.find_by(item_id: @item.id).destroy
+        respond_to do |format|
+            format.html { redirect_to request.referrer || root_url }
+            format.js
+          end
+    end
 
 end
