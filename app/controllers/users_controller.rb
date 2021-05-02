c59c217344015499646cfcc02beed83fd5815abe
kenken-9 <1508.parfum@gmail.com>
2021-05-02 12:58:52 +0900

    ✨お気に入り一覧を表示

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def show
-    
+    @user = User.find(params[:id])
+    @items = @user.items
+
+    bookmarks = Bookmark.where(user_id: current_user.id).pluck(:item_id)
+    @bookmark_list = Item.find(bookmarks)
   end
+
+
 end
