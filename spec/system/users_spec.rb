c91b244da4461bc59ce4fc2849532c5ff73589e4
kenken-9 <1508.parfum@gmail.com>
2021-05-09 07:25:05 +0900

    ✅ 新規登録できない時のテスト

--- spec/system/users_spec.rb
+++ spec/system/users_spec.rb
@@ -36,7 +36,32 @@ RSpec.describe "Users", type: :system do
       # サインアップページへ遷移するボタンや、ログインページへ遷移するボタンが表示されていないことを確認する
       expect(page).to have_no_content("新規登録")
       expect(page).to have_no_content("ログイン")
+    end
 
+    it "誤った情報ではユーザー新規登録ができずに新規登録ページに戻ってくる" do
+      # トップページに移動する
+      visit root_path
+      
+      # トップページにサインアップページへ遷移するボタンがある
+      expect(page).to have_content("新規登録")
+
+      # 新規登録ページに移動する
+      visit new_user_registration_path
+
+      # ユーザー情報を入力する
+      fill_in "nickname", with: ""
+      fill_in "email", with: ""
+      fill_in "password" , with: ""
+      fill_in "password_confirmation" , with: ""
+      fill_in "profile" , with: ""
+
+      # サインアップボタンを押してもユーザーモデルのカウントは上がらないことを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change {User.count}.by(0)
+
+      # 新規登録ページに戻されることを確認
+      expect(current_path).to eq user_registration_path
     end
   end
 end
