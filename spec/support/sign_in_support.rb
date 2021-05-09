76cca7552f08c2892d6a660c82228f0d67ffa920
kenken-9 <1508.parfum@gmail.com>
2021-05-09 21:26:59 +0900

    ✅商品の削除ができない時のテストコード

--- spec/support/sign_in_support.rb
+++ spec/support/sign_in_support.rb
@@ -6,4 +6,13 @@ module SignInSupport
     find('input[name="commit"]').click
     expect(current_path).to eq(root_path)
   end
+
+  def sign_in1(user)
+    visit new_user_session_path
+      fill_in "email", with: @item1.user.email
+      fill_in "password", with: @item1.user.password
+      find('input[name="commit"]').click
+      expect(current_path).to eq(root_path)
+  end
+
 end
